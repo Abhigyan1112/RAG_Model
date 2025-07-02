3f8b76768a699b6935d19578260ec008ec6f0067
Abhigyan1112 <abhigyansharma.hhh@gmail.com>
2025-07-02 18:11:43 +0530

    switched to groq llama3

--- RAGModel.py
+++ RAGModel.py
@@ -1,53 +1,32 @@
-#!/usr/bin/env python
-# coding: utf-8
 
-from pyngrok import ngrok
 import requests
-from flask import Flask, request, jsonify, render_template, flash, url_for, redirect
+from flask import Flask, request, jsonify, render_template, url_for, redirect
 import fitz
 from sentence_transformers import SentenceTransformer
 import chromadb
 from chromadb.utils import embedding_functions
 from chromadb.config import Settings
-from flask_sqlalchemy import SQLAlchemy
 import numpy as np
 import json
-
+from io import BytesIO
+from flask_sqlalchemy import SQLAlchemy
 
 app=Flask(__name__)
 app.secret_key = 'aditi18'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///RAG.db'
 db=SQLAlchemy(app)
 
-
-class PDF(db.Model):
-    filename=db.Column(db.String(200),nullable=False)
-    id=db.Column(db.Integer,primary_key=True,autoincrement=True)
-    file=db.Column(db.LargeBinary,nullable=False)
+class FILE(db.Model):
+    sno = db.Column(db.Integer,primary_key=True,autoincrement=True)
+    file = db.Column(db.LargeBinary,nullable=False)
 
     def __repr__(self) -> str:
-        return f'{self.file} - {self.id}'
-
-
-#Here we are exposing the Ollama server to the internet with the help of ngrok
-# public_url=ngrok.connect(11434,"http",host_header="localhost:11434").public_url
-# print(public_url)
-public_url="http://localhost:11434"
-
+        return f'{self.sno}'
 
 @app.route("/")
 def main():
-    #sending an HTTP get request to the server api tags
-    response=requests.get(public_url+'/api/tags')
-    try:
-        if response.status_code==200:
-                print("The public_url is successfully working")
-        else:
-            print("Status Code:",response.status_code)
-    except Exception as e:
-        print("Error",e)
     full_response=request.args.get("full_response")
-    return render_template('index.html',response=full_response or " ")
+    return render_template('index.html',response=full_response or "")
 
 
 @app.route("/Answer", methods=['POST'])
@@ -55,14 +34,31 @@ def answer():
     print("FILES RECEIVED:", request.files)
     file = request.files['file']
     query= request.form['query']
+    filename=file.filename
+    file=file.read()
+    
+    if not filename=='':
+        db.session.query(FILE).delete()
+        db.session.commit()
+        entry=FILE(file=file)
+        print("File is given")
+        db.session.add(entry)
+        db.session.commit()
+    else:
+        print("file is not given")
+        file=db.session.query(FILE.file).distinct().all()
+        file=file[0][0]
+
+    file_stream=BytesIO(file)
+    chunks = extract_chunks_from_pdf(file_stream)
 
     model = SentenceTransformer("all-MiniLM-L6-v2")
     client=chromadb.Client()
-    filename=file.filename
-    collection = client.get_or_create_collection(name="{filename}") #Collection is like a vector table for the document
-    file_path = f"static/files/{filename}"
-    file.save(file_path)
-    chunks = extract_chunks_from_pdf(file_path)
+    try:
+        client.delete_collection(name="abcd")
+    except Exception as e:
+        print(f"Collection delete failed or does not exist: {e}")
+    collection = client.get_or_create_collection(name="abcd") #Collection is like a vector table for the document
 
     try:
         collection.add(
@@ -82,33 +78,35 @@ def answer():
     relevant_chunks = results['documents'][0]
     context = "\n".join(relevant_chunks)
 
-    url=f'{public_url}/api/generate'
-    headers={"Content-Type":"application/json"}
-    data={"model":"mistral",
-        "prompt":f"Context:{context}\n\n Question:{query}\n Answer:"}
+    url='https://api.groq.com/openai/v1/chat/completions'
 
-    response = requests.post(url, json=data, headers=headers, stream=True)
+    headers={
+        "Content-Type":"application/json",
+        "Authorization":"Bearer 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"
+    }
+
+    data = {
+        "model": "llama3-8b-8192",
+        "messages":[
+            {"role":"system","content":f"Context:{context}"},
+            {"role":"user","content":query}
+        ]
+    }
+
+    response = requests.post(url, json=data, headers=headers)
     full_response=""
     if response.status_code == 200:
-        for line in response.iter_lines():
-            if line:
-                try:
-                    decoded_line = line.decode("utf-8")
-                    json_obj = json.loads(decoded_line)
-                    full_response += json_obj.get("response", "")
-                except json.JSONDecodeError:
-                    continue
+        full_response=response.json()["choices"][0]["message"]["content"]
     else:
         raise Exception(f"Failed to generate response: {response.status_code}")
 
-    full_response=full_response.strip()
-    print(full_response)
+    # full_response=full_response.strip()
     return redirect(url_for('main',full_response=full_response))
 
 
 
-def extract_chunks_from_pdf(pdf_path,chunk_size=500):
-    doc=fitz.open(pdf_path)
+def extract_chunks_from_pdf(file,chunk_size=500):
+    doc=fitz.open(stream=file,filetype='pdf')
     all_text=' '
 
     for page in doc:
